a0470b96d538fecb6b97ed2f0ce137e962239c97
Duff O'Melia <duff@omelia.org>
2016-01-10 14:54:38 -0500

    Bump the version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Spreedly.Mixfile do
 
   def project do
     [app: :spreedly,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
