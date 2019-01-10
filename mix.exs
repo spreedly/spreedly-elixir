ac8176ddeabf2a7462f63d53e76c6a3b3d24cc25
Jared Knipp <jared.r.knipp@gmail.com>
2019-01-10 13:55:28 -0600

    Version 2.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spreedly.Mixfile do
   def project do
     [
       app: :spreedly,
-      version: "2.1.0",
+      version: "2.1.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
