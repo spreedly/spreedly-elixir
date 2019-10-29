aa675f03ad3dbf98acd40daa0ab199eca5656a83
Niaja <niaja@spreedly.com>
2019-10-29 10:55:37 -0400

    Version 2.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Spreedly.Mixfile do
   def project do
     [
       app: :spreedly,
-      version: "2.1.2",
+      version: "2.1.3",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
